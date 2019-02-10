068c227c17d26cf8edc704924e920a7244292ebc
Ruslan Gonzalez <ruslanguns@gmail.com>
2019-02-10 14:04:21 +0000

    Mejoras de validacion de es numerico

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ module.exports = {
      * @param { number } n2 Número para sumar
      */
     suma: function(n1, n2) {
-        return n1 + n2;
+        // return n1 + n2;
+        return (this.esNumero(n1, n2)) ? n1 + n2 : this.mensajeError();
     },
     /**
      * Resta entre números
@@ -15,7 +16,7 @@ module.exports = {
      * @param { number } n2 Número para restar
      */
     resta: function(n1, n2) {
-        return n1 - n2;
+        return (this.esNumero(n1, n2)) ? n1 - n2 : this.mensajeError();
     },
     /**
      * Multiplicacion entre números
@@ -23,14 +24,24 @@ module.exports = {
      * @param { number } n2 Número para multiplicar
      */
     multiplicacion: function(n1, n2) {
-        return n1 * n2;
+        return (this.esNumero(n1, n2)) ? n1 * n2 : this.mensajeError();
     },
     /**
-     * Suma entre números
-     * @param { number } n1 Número para dividormultiplicar
-     * @param { number } n2 Número para dividormultiplicar
+     * División entre números
+     * @param { number } n1 Número para división
+     * @param { number } n2 Número para división
      */
     division: function(n1, n2) {
-        return n1 / n2;
+        return (this.esNumero(n1, n2)) ? n1 / n2 : this.mensajeError();
+    },
+    esNumero: function(n1, n2) {
+        if (typeof n1 != 'number' || typeof n2 != 'number') {
+            return false;
+        }
+        return true;
+    },
+    mensajeError: function() {
+        console.log('Uno o más de los valores introducidos no son de tipo numérico.');
+
     }
 }
