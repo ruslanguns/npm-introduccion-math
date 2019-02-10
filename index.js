b5a4cd8ec24fc767d477f41273ca7ad0957abc7c
Ruslan Gonzalez <ruslanguns@gmail.com>
2019-02-10 01:08:51 +0000

    Añadido módulo principal del proyecto

---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = {
+    /**
+     * Suma entre números
+     * @example
+     * 1 + 1 = 2
+     * @param { number } n1 Número para sumar
+     * @param { number } n2 Número para sumar
+     */
+    suma: function(n1, n2) {
+        return n1 + n2;
+    },
+    /**
+     * Resta entre números
+     * @param { number } n1 Número para restar
+     * @param { number } n2 Número para restar
+     */
+    resta: function(n1, n2) {
+        return n1 - n2;
+    },
+    /**
+     * Multiplicacion entre números
+     * @param { number } n1 Número para multiplicar
+     * @param { number } n2 Número para multiplicar
+     */
+    multiplicacion: function(n1, n2) {
+        return n1 * n2;
+    },
+    /**
+     * Suma entre números
+     * @param { number } n1 Número para dividormultiplicar
+     * @param { number } n2 Número para dividormultiplicar
+     */
+    division: function(n1, n2) {
+        return n1 / n2;
+    }
+}
